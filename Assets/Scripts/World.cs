81d6b5588e860af05c422600a719fa8a489f6e26
Fab <mail@fabschneider.com>
2021-11-21 14:45:37 +0100

    Add bounds calculation in chunk generation

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -10,14 +10,14 @@ namespace Fab.Geo
 {
     public class World : MonoBehaviour
     {
-        [Tooltip("Mesh resolution of the generated chunks. Number relates to the number of quads for each row/column of the chunk. Must be multiple of two.")]
-        public int chunkResolution = 4;
+        [Tooltip("Mesh resolution of the generated chunks. Number relates to the number of quads for each row/column of the chunk.")]
+        [Range(1, 255)]
+        public int chunkResolution = 127;
         [Tooltip("Number of chunks per row/column on each side of the sphere. Must be multiple of 2 and cannot be bigger than the chunk resolution.")]
-        public int chunkCount = 2;
+        [Range(1, 16)]
+        public int chunkCount = 8;
         [Tooltip("The chunk prefab to instantiate")]
         public MeshFilter chunkPrefab;
-
-
 
         private void Start()
         {
@@ -28,12 +28,6 @@ namespace Fab.Geo
             Debug.Log($"Generated world with {chunkCount * chunkCount * 6} chunks @ res {chunkResolution} in {(stopwatch.ElapsedMilliseconds / 1000.0):0.00s}");
         }
 
-        private void OnValidate()
-        {
-            chunkResolution = math.max(1, chunkResolution);
-            chunkCount = math.max(1, chunkCount);
-        }
-
         [Button("Generate World")]
         private void GenerateWorld()
         {
@@ -42,7 +36,7 @@ namespace Fab.Geo
             worldChunksParent.transform.SetParent(transform, false);
             CreateWorldChunkObjects(worldChunksParent.transform, chunkPrefab, meshDataArray, chunkCount);
         }
-        
+
         private static void CreateWorldChunkObjects(Transform parent, MeshFilter chunkPrefab, Mesh.MeshDataArray meshDataArray, int chunkCount)
         {
             Mesh[] meshes = new Mesh[meshDataArray.Length];
@@ -55,7 +49,9 @@ namespace Fab.Geo
                 for (int chunk = 0; chunk < chunksPerFace; chunk++)
                 {
                     Mesh m = new Mesh();
-                    m.name = $"WorldChunk_x{chunk % chunkCount}.y{chunk / chunkCount}.f{face}";
+                    int3 chunkId = new int3(chunk % chunkCount, chunk / chunkCount, face);
+                    m.name = $"WorldChunk_x{chunkId.x}.y{chunkId.y}.f{chunkId.z}";
+                    m.bounds = CalculateChunkBounds(chunkId, 2f / chunkCount);
                     meshes[face * chunksPerFace + chunk] = m;
                 }
             }
@@ -75,34 +71,29 @@ namespace Fab.Geo
 
         private static Mesh.MeshDataArray GenerateWorldMeshes(int chunkResolution, int chunkCount)
         {
-            //make sure the resolution and chunk count is a multiple of 2
-            //chunkResolution = math.max(2, chunkResolution - chunkResolution % 2);
-            //chunkCount = math.clamp(chunkCount - chunkCount % 2, 2, chunkResolution);
+            if (chunkResolution < 1 || chunkResolution > 255)
+                Debug.LogError("Chunk resolution has to be in between 1 and 255");
+
+            chunkResolution = math.clamp(chunkResolution, 1, 255);  
 
             Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(chunkCount * chunkCount * 6);
 
             NativeArray<JobHandle> jobs = new NativeArray<JobHandle>(6, Allocator.TempJob);
 
             for (int i = 0; i < 6; i++)
-                jobs[i] = GenerateFace(i, meshDataArray, chunkResolution, chunkCount);
+                jobs[i] = GenerateFace(i, meshDataArray, (ushort)chunkResolution, (ushort)chunkCount);
 
             JobHandle.CombineDependencies(jobs).Complete();
             jobs.Dispose();
             return meshDataArray;
         }
 
-        private static JobHandle GenerateFace(int face, Mesh.MeshDataArray meshDataArray, int chunkResolution, int chunkCount)
+        private static JobHandle GenerateFace(int face, Mesh.MeshDataArray meshDataArray, ushort chunkResolution, ushort chunkCount)
         {
-            float chunkExtent = 1f / chunkCount;
-
-            float3 faceNormal = MeshUtils.cubeFaces[face];
-            float3 faceAxisY = new float3(faceNormal.y, faceNormal.z, faceNormal.x);
-            float3 faceAxisX = math.cross(faceNormal, faceAxisY);
-
-            float3x3 faceTransform = new float3x3(faceAxisX, faceAxisY, faceNormal);
+            float chunkSize = 2f / chunkCount;
+            float3x3 faceTransform = GetFaceTransform(face);
 
             int chunkBaseIndex = face * chunkCount * chunkCount;
-
             NativeArray<JobHandle> chunkJobs = new NativeArray<JobHandle>(meshDataArray.Length, Allocator.TempJob);
 
             //iterate each chunk of the face
@@ -121,20 +112,18 @@ namespace Fab.Geo
                         new VertexAttributeDescriptor(VertexAttribute.TexCoord0, dimension: 2, stream: 2));
 
                     int chunkIndexCount = chunkResolution * chunkResolution * 6;
-                    meshData.SetIndexBufferParams(chunkIndexCount, IndexFormat.UInt32);
+                    meshData.SetIndexBufferParams(chunkIndexCount, IndexFormat.UInt16);
 
                     GenerateWorldChunkJob chunkJob = new GenerateWorldChunkJob()
                     {
                         meshData = meshData,
-                        face = face,
                         faceTransform = faceTransform,
-                        chunkId = new int2(x, y),
+                        chunkId = new int3(x, y, face),
                         chunkResolution = chunkResolution,
-                        chunkExtent = chunkExtent
+                        chunkSize = chunkSize
                     };
 
                     chunkJobs[index] = chunkJob.Schedule();
-                   // GenerateWorldChunk(meshData, face, new int2(x, y), chunkResolution, chunkExtent, faceTransform);
                 }
             }
 
@@ -143,88 +132,37 @@ namespace Fab.Geo
             return combinedJobs;
         }
 
-        private static void GenerateWorldChunk(Mesh.MeshData meshData, int face, int2 chunkId, int chunkResolution, float chunkExtent, float3x3 faceTransform)
+        private static float3x3 GetFaceTransform(int face)
         {
-            float chunkDimension = chunkExtent * 2;
-            meshData.subMeshCount = 1;
+            float3 faceNormal = MeshUtils.cubeFaces[face];
+            float3 faceAxisY = new float3(faceNormal.y, faceNormal.z, faceNormal.x);
+            float3 faceAxisX = math.cross(faceNormal, faceAxisY);
 
-            int chunkVertexCount = (chunkResolution + 1) * (chunkResolution + 1);
+            return new float3x3(faceAxisX, faceAxisY, faceNormal);
+        }
 
-            meshData.SetVertexBufferParams(chunkVertexCount,
-                new VertexAttributeDescriptor(VertexAttribute.Position),
-                new VertexAttributeDescriptor(VertexAttribute.Normal, stream: 1),
-                new VertexAttributeDescriptor(VertexAttribute.TexCoord0, dimension: 2, stream: 2));
+        private static Bounds CalculateChunkBounds(int3 chunkId, float chunkSize)
+        {
+            float3x3 faceTransform = GetFaceTransform(chunkId.z);
 
-            int chunkIndexCount = chunkResolution * chunkResolution * 6;
+            float3 p00 = GeoUtils.PointOnCubeToPointOnSphere(faceTransform.c2
+                + faceTransform.c0 * chunkSize * chunkId.x - faceTransform.c0
+                + faceTransform.c1 * chunkSize * chunkId.y - faceTransform.c1);
+            float3 p01 = GeoUtils.PointOnCubeToPointOnSphere(faceTransform.c2
+                + faceTransform.c0 * chunkSize * chunkId.x - faceTransform.c0
+                + chunkSize * faceTransform.c1 + faceTransform.c1 * chunkSize * chunkId.y - faceTransform.c1);
+            float3 p10 = GeoUtils.PointOnCubeToPointOnSphere(faceTransform.c2
+                + chunkSize * faceTransform.c0 + faceTransform.c0 * chunkSize * chunkId.x - faceTransform.c0
+                + faceTransform.c1 * chunkSize * chunkId.y - faceTransform.c1);
+            float3 p11 = GeoUtils.PointOnCubeToPointOnSphere(faceTransform.c2
+                + chunkSize * faceTransform.c0 + faceTransform.c0 * chunkSize * chunkId.x - faceTransform.c0
+                + chunkSize * faceTransform.c1 + faceTransform.c1 * chunkSize * chunkId.y - faceTransform.c1);
 
-            meshData.SetIndexBufferParams(chunkIndexCount, IndexFormat.UInt32);
-
-            var positions = meshData.GetVertexData<float3>();
-            var normals = meshData.GetVertexData<float3>(1);
-            var uv = meshData.GetVertexData<float2>(2);
-            var indices = meshData.GetIndexData<int>();
-
-            int vertexID = 0;
-            int indexID = 0;
-
-            //populate vertices
-            for (int y = 0; y <= chunkResolution; y++)
-            {
-                for (int x = 0; x <= chunkResolution; x++)
-                {
-                    float2 chunkParam = new float2(
-                        x / (float)chunkResolution,
-                        y / (float)chunkResolution);
-
-                    float3 p = faceTransform.c2
-                                + chunkParam.x * chunkDimension * faceTransform.c0 + faceTransform.c0 * chunkDimension * chunkId.x - faceTransform.c0
-                                + chunkParam.y * chunkDimension * faceTransform.c1 + faceTransform.c1 * chunkDimension * chunkId.y - faceTransform.c1;
-
-                    p = GeoUtils.PointOnCubeToPointOnSphere(p);
-
-                    positions[vertexID] = p;
-                    normals[vertexID] = p;
-
-                    float2 texCoord = GeoUtils.NormalizeCoordinate(GeoUtils.PointToCoordinate(p));
-
-                    //handle seam wrapping where the latitude is -180/180
-                    if (texCoord.x == 1f)
-                    {
-                        if ((face == 0 && x == 0)  //front face
-                        ||  (face == 1 && y == 0)  //bottom face
-                        ||  (face == 4 && y != 0)) //top face
-                        {
-                            texCoord.x = 0f;
-
-                            if (texCoord.y == 0f || texCoord.y == 1f)
-                                texCoord.x = 0.25f;
-                        }
-                        else if (texCoord.y == 0f || texCoord.y == 1f)
-                        {
-                            texCoord.x = 0.75f;
-                        }
-                    }
-                    uv[vertexID] = texCoord;
-                    vertexID++;
-                }
-            }
-
-            //populate indices
-            for (int y = 0; y < chunkResolution; y++)
-            {
-                for (int x = 0; x < chunkResolution; x++)
-                {
-                    int posId = x + y * (chunkResolution + 1);
-                    indices[indexID++] = posId;
-                    indices[indexID++] = (posId + chunkResolution + 1);
-                    indices[indexID++] = (posId + chunkResolution + 2);
-                    indices[indexID++] = posId;
-                    indices[indexID++] = (posId + chunkResolution + 2);
-                    indices[indexID++] = (posId + 1);
-                }
-            }
-
-            meshData.SetSubMesh(0, new SubMeshDescriptor(0, indices.Length));
+            Bounds b = new Bounds(p00, Vector3.zero);
+            b.Encapsulate(p01);
+            b.Encapsulate(p10);
+            b.Encapsulate(p11);
+            return b;
         }
     }
 
@@ -232,35 +170,35 @@ namespace Fab.Geo
     public struct GenerateWorldChunkJob : IJob
     {
         public Mesh.MeshData meshData;
-        public int face;
         public float3x3 faceTransform;
-        public int2 chunkId;
-        public int chunkResolution;
-        public float chunkExtent;
+        /// <summary>
+        /// x: face column id, y: face row id, z: face id
+        /// </summary>
+        public int3 chunkId;
+        public ushort chunkResolution;
+        public float chunkSize;
         public void Execute()
         {
-            float chunkDimension = chunkExtent * 2;
-
             var positions = meshData.GetVertexData<float3>();
             var normals = meshData.GetVertexData<float3>(1);
             var uv = meshData.GetVertexData<float2>(2);
-            var indices = meshData.GetIndexData<int>();
+            var indices = meshData.GetIndexData<ushort>();
 
             int vertexID = 0;
             int indexID = 0;
 
             //populate vertices
-            for (int y = 0; y <= chunkResolution; y++)
+            for (ushort y = 0; y <= chunkResolution; y++)
             {
-                for (int x = 0; x <= chunkResolution; x++)
+                for (ushort x = 0; x <= chunkResolution; x++)
                 {
                     float2 chunkParam = new float2(
                         x / (float)chunkResolution,
                         y / (float)chunkResolution);
 
                     float3 p = faceTransform.c2
-                                + chunkParam.x * chunkDimension * faceTransform.c0 + faceTransform.c0 * chunkDimension * chunkId.x - faceTransform.c0
-                                + chunkParam.y * chunkDimension * faceTransform.c1 + faceTransform.c1 * chunkDimension * chunkId.y - faceTransform.c1;
+                                + chunkParam.x * chunkSize * faceTransform.c0 + faceTransform.c0 * chunkSize * chunkId.x - faceTransform.c0
+                                + chunkParam.y * chunkSize * faceTransform.c1 + faceTransform.c1 * chunkSize * chunkId.y - faceTransform.c1;
 
                     p = GeoUtils.PointOnCubeToPointOnSphere(p);
 
@@ -272,9 +210,9 @@ namespace Fab.Geo
                     //handle seam wrapping where the latitude is -180/180
                     if (texCoord.x == 1f)
                     {
-                        if ((face == 0 && x == 0)  //front face
-                        || (face == 1 && y == 0)  //bottom face
-                        || (face == 4 && y != 0)) //top face
+                        if ((chunkId.z == 0 && x == 0)  //front face
+                        || (chunkId.z == 1 && y == 0)  //bottom face
+                        || (chunkId.z == 4 && y != 0)) //top face
                         {
                             texCoord.x = 0f;
 
@@ -292,21 +230,21 @@ namespace Fab.Geo
             }
 
             //populate indices
-            for (int y = 0; y < chunkResolution; y++)
+            for (ushort y = 0; y < chunkResolution; y++)
             {
-                for (int x = 0; x < chunkResolution; x++)
+                for (ushort x = 0; x < chunkResolution; x++)
                 {
-                    int posId = x + y * (chunkResolution + 1);
+                    ushort posId = (ushort)(x + y * (chunkResolution + 1));
                     indices[indexID++] = posId;
-                    indices[indexID++] = (posId + chunkResolution + 1);
-                    indices[indexID++] = (posId + chunkResolution + 2);
+                    indices[indexID++] = (ushort)(posId + chunkResolution + 1);
+                    indices[indexID++] = (ushort)(posId + chunkResolution + 2);
                     indices[indexID++] = posId;
-                    indices[indexID++] = (posId + chunkResolution + 2);
-                    indices[indexID++] = (posId + 1);
+                    indices[indexID++] = (ushort)(posId + chunkResolution + 2);
+                    indices[indexID++] = (ushort)(posId + 1);
                 }
             }
-
             meshData.SetSubMesh(0, new SubMeshDescriptor(0, indices.Length));
         }
     }
+
 }
