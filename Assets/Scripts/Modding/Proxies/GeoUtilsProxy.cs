1dc081b1d4037a657f40fdf3e4b78c682dbe9f33
Fab <mail@fabschneider.com>
2021-11-30 00:51:44 +0100

    Add geo proxy

---
+++ Assets/Scripts/Modding/Proxies/GeoUtilsProxy.cs
@@ -0,0 +1,16 @@
+using MoonSharp.Interpreter;
+using Unity.Mathematics;
+
+namespace Fab.Geo.Modding
+{
+    [MoonSharpUserData]
+    public class GeoProxy
+    {
+        public float distance(float lat_1, float lon_1, float lat_2, float lon_2)
+        {
+            return GeoUtils.Distance(
+                new Coordinate(math.radians(lon_1), math.radians(lat_1)), 
+                new Coordinate(math.radians(lon_2), math.radians(lat_2)));
+        }
+    }
+}
