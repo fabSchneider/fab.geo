31b7a675d830b03631402e98c702b43385f2c787
Fab <mail@fabschneider.com>
2021-12-03 16:31:14 +0100

    Add test suite

---
+++ Assets/Tests/RuntimeTests/PlaceholderTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class PlaceholderTest
+{
+    [Test]
+    public void Placeholder_Test()
+    {
+        Debug.Log("Placeholder test in play mode");
+    }
+}
