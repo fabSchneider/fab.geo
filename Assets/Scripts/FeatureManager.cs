6bb024a70f3cb12b217b67a0c4b44007ce553399
Fab <mail@fabschneider.com>
2021-12-21 11:17:57 +0100

    Add GetFeature method

--- Assets/Scripts/FeatureManager.cs
+++ Assets/Scripts/FeatureManager.cs
@@ -5,16 +5,41 @@ namespace Fab.Geo.Modding
 {
     public class FeatureManager : MonoBehaviour
     {
-        private List<Feature> features;
 
         [SerializeField]
         private FeaturePoint pointPrefab;
         [SerializeField]
         private FeatureLine linePrefab;
 
+        private Dictionary<string, List<Feature>> features;
+
         private void Awake()
         {
-            features = new List<Feature>();
+            features = new Dictionary<string, List<Feature>>();
+        }
+
+        /// <summary>
+        /// Gets the first feature with given name or null if no feature with that name exists
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Feature GetFeature(string name)
+        {
+            if (features.TryGetValue(name, out List<Feature> list))
+                return list[0];
+            return null;
+        }
+
+        /// <summary>
+        /// Gets all features with the given name or null if no feature with that name exists
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public IEnumerable<Feature> GetFeatures(string name)
+        {
+            if (features.TryGetValue(name, out List<Feature> list))
+                return list;
+            return null;
         }
 
         public FeaturePoint AddPoint(string name, Coordinate coord)
@@ -24,7 +49,7 @@ namespace Fab.Geo.Modding
             //add some altitude so the point is not sunken into the ground
             inst.transform.localPosition = pos + pos * 0.005f;
             inst.name = name;
-            features.Add(inst);
+            AddFeature(inst);
             return inst;
         }
 
@@ -33,18 +58,34 @@ namespace Fab.Geo.Modding
             FeatureLine inst = Instantiate(linePrefab, transform);
             inst.name = name;
             inst.SetEndpoints(a, b);
-            features.Add(inst);
+            AddFeature(inst);
             return inst;
         }
 
         public bool RemoveFeature(Feature feature)
         {
-            if (features.Remove(feature))
+            if (features.TryGetValue(feature.name, out List<Feature> list))
             {
-                Destroy(feature.gameObject);
-                return true;
+                if (list.Remove(feature))
+                {
+                    Destroy(feature.gameObject);
+                    return true;
+                }
             }
             return false;
+        }
+
+        private void AddFeature(Feature feature)
+        {
+            if(features.TryGetValue(feature.name, out List<Feature> list))
+            {
+                if (!list.Contains(feature))
+                    list.Add(feature);
+            }
+            else
+            {
+                features.Add(feature.name, new List<Feature>() { feature });
+            }
         }
     }
 }
