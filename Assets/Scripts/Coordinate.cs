232b084022c08f18e78be54e7736b6e7f13fbce5
Fab <mail@fabschneider.com>
2022-01-16 14:44:57 +0100

    Add more operator overloads

--- Assets/Scripts/Coordinate.cs
+++ Assets/Scripts/Coordinate.cs
@@ -1,9 +1,11 @@
+using System;
+using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 
 namespace Fab.Geo
 {
     [System.Serializable]
-    public struct Coordinate
+    public struct Coordinate : IEquatable<Coordinate>, IFormattable
     {
         public float longitude;
         public float latitude;
@@ -14,19 +16,39 @@ namespace Fab.Geo
             this.latitude = latitude;
         }
 
-        public static implicit operator float2(Coordinate c)
-        {
-            return new float2(c.latitude, c.longitude);
-        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator float2(Coordinate c) { return new float2(c.latitude, c.longitude); }
 
-        public static implicit operator Coordinate(float2 f2)
-        {
-            return new Coordinate(f2.x, f2.y);
-        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator Coordinate(float2 f2) { return new Coordinate(f2.x, f2.y); }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool2 operator ==(Coordinate lhs, Coordinate rhs) { return new bool2(lhs.longitude == rhs.longitude, lhs.latitude == rhs.latitude); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool2 operator !=(Coordinate lhs, Coordinate rhs) { return new bool2(lhs.longitude != rhs.longitude, lhs.latitude != rhs.latitude); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Coordinate rhs) { return longitude == rhs.longitude && latitude == rhs.latitude; }
+
+        public override bool Equals(object o) { return o is Coordinate converted && Equals(converted); }
+
+
+        /// <summary>Returns a hash code for the float2.</summary>
+        /// <returns>The computed hash code.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() { return (int)math.hash(this); }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override string ToString()
         {
-            return $"(Lat: {math.degrees(latitude)} Lon: {math.degrees(longitude)})";
+            return string.Format("(Lon: {0} Lat: {1})", math.degrees(longitude), math.degrees(latitude));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return string.Format("(Lon: {0} Lat: {1})", longitude.ToString(format, formatProvider), latitude.ToString(format, formatProvider));
         }
     }
 }
