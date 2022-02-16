522f392707611c3f6a902f28612d2a801f9d6162
Fab <mail@fabschneider.com>
2022-02-16 17:06:23 +0100

    Add open scripts dir button

--- Assets/Scripts/UI/ReloadUI.cs
+++ Assets/Scripts/UI/ReloadUI.cs
@@ -1,3 +1,6 @@
+using Fab.Geo.Lua.Core;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,8 +14,33 @@ namespace Fab.Geo.UI
         void Start()
         {
             document = GetComponent<UIDocument>();
-            Button reloadBtn = document.rootVisualElement.Q<Button>(name: "reload-btn");
-            reloadBtn.clicked += App.Reload;      
+            document.rootVisualElement.Q<Button>(name: "reload-btn").clicked += App.Reload;
+
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            document.rootVisualElement.Q<Button>(name: "open-scripts-btn").clicked += () => OpenExplorer(LuaEnvironment.ScriptsDirectory);
+#else
+            document.rootVisualElement.Q<Button>(name: "open-scripts-btn").RemoveFromHierarchy();     
+#endif
+        }
+
+        private void OpenExplorer(string directory)
+        {
+            if (!Directory.Exists(directory))
+                return;
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+
+#endif
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                Arguments = directory,
+                FileName = "explorer.exe"
+            };
+
+            Process.Start(startInfo);
+
         }
     }
 }
